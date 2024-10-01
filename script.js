eafcceed24514807da1a4aaf37a6f6965ca5975c
kimdogbe <kimdogbe@gmail.com>
2024-10-01 16:47:12 +0100

    Create operate function and variables

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let num1 = 0;
+let num2 = 0;
+let operator = '';
+
+function operate(operator, num1, num2){
+  switch (operator){
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "x":
+      multiply(num1, num2);
+      break;
+    case "/":
+      devide(num1, num2);
+      break;
+  }
+}
