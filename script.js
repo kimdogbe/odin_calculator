7b954363cd5aa538f981914763fc91c67d6c8d5d
kimdogbe <kimdogbe@gmail.com>
2024-10-02 13:25:35 +0100

    Put in first round of logic

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function add(a, b) {
-	return a + b;
+	return Number(a) + Number(b);
 };
 
 function subtract(a, b) {
-	return a - b;
+	return Number(a) - Number(b);
 };
 
 function multiply(a, b) {
-  return a * b;
+  return Number(a) * Number(b);
 };
 
 function divide(a, b) {
-  return a / b;
+  return Number(a) / Number(b);
 }
 
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operator = "";
 
 let displayValue = "";
@@ -23,16 +23,69 @@ let displayValue = "";
 function operate(operator, num1, num2){
   switch (operator){
     case "+":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "-":
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case "x":
-      multiply(num1, num2);
-      break;
+      return multiply(num1, num2);
     case "/":
-      devide(num1, num2);
-      break;
+      return divide(num1, num2);
   }
 }
+
+function clearDisplay() {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  displayValue = 0;
+  display.textContent = "0";
+}
+
+function setNumber(num){
+  if (operator == ""){
+    num1 = num1 + num;
+    return num1;
+  } 
+  else {
+    num2 = num2 + num;
+    return num2
+  }
+}
+
+function setOperator(op) {
+  if (operator == ""){
+    operator = op;
+    return operator;
+  }
+}
+
+// html and eventListeners
+const display = document.querySelector("#display");
+
+addEventListener("click", (event) => {
+  let buttonClicked = event.target.innerHTML;
+  let buttonClasses = event.target.className;
+
+  
+    if (buttonClasses.includes("number")){
+
+      displayValue = setNumber(buttonClicked);
+      display.textContent = displayValue;
+    }
+    else if (buttonClasses.includes("operand")){
+      displayValue = setOperator(buttonClicked);
+      display.textContent = displayValue;
+    }
+    else if (buttonClasses.includes("clear")){
+      clearDisplay();
+    }
+    else if (buttonClasses.includes("eqlBtn")) {
+      displayValue = operate(operator, num1, num2);
+      display.textContent = displayValue;
+    }
+  
+  // if (event.target.className.includes("btn")){
+  //   displayValue = event.target.innerHTML;
+  //   display.textContent = displayValue;
+  // }
+});
